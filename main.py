b8e33a0cc94ff08ab81987fe55c2dd1b28b015c3
legendo1234 <matej.weber@gmail.com>
2025-04-23 13:28:58 +0200

    added movement to triangle player object, and added groups

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
@@ -17,18 +18,33 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return  # Exit the loop and close the game
 
         screen.fill("black")  # Fill the screen with a solid color
+
+        # Call update for all updatable objects
+        updatable.update(dt)
+
+        # Loop over all drawable objects and call their draw method
+        for sprite in drawable:
+            sprite.draw(screen)
+
         pygame.display.flip()  # Refresh the display
 
-        
-        miliseconds = clock.tick(60)
-        dt = miliseconds / 1000
+        # Frame rate control
+        milliseconds = clock.tick(60)
+        dt = milliseconds / 1000
 
+    
 
 
 
